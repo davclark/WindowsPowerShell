c5f54de5ebf72ad73cb387621476db36fe8b190d
Dav Clark <dav@bead.glass>
2018-06-07 12:43:01 -0400

    Basic choco + silent

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+﻿Set-PSReadlineOption -BellStyle None
+
+# Chocolatey profile
+$ChocolateyProfile = "$Env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
+
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
---
+++ README.md
@@ -0,0 +1,5 @@
+WindowsPowerShell config dir
+============================
+
+This is my (rather simple) windows powershell config dir. It goes in Documents
+(per the curious Microsoft logic that all manner of things go in Documents).
